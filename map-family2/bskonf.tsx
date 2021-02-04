e06a7f968510c2ad15b292d206052b4b37f6cdb5
twittpahl <thomas@wittpahl.com>
2021-02-04 20:59:59 +0100

    copied family

---
+++ map-family2/bskonf.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="bskonf" tilewidth="32" tileheight="32" tilecount="450" columns="30">
+ <image source="bskonf.png" width="962" height="508"/>
+</tileset>
